b591d4c906396e70cd0a231af315b31643c4da00
Muthiani <bluntcoder254@gmail.com>
2025-04-28 10:36:09 +0300

    Feat: Designed shop category to mmirror different categories

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import Item from '../Components/Item/Item'
+import { ShopContext } from '../Context/ShopContext'
 
-const ShopCategory = () => {
+const ShopCategory = (props) => {
+
+  const { all_product } = useContext(ShopContext)
+
   return (
-    <div>shopCategory</div>
+    <div className="ShopCategory">
+      <img className="shopcategory-banner" src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1 to 12</span> out of 36 products.
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product.map((item, i) => {
+          if (item.category === props.category) {
+            return (
+              <Item key={i} image={item.image} name={item.name} new_price={item.new_price} old_price={item.old_price}/>
+            )
+          } else {
+          return null;
+          }
+        })}
+      </div>
+      <dic className="shopcategory-loadmore">
+        Explore More
+      </dic>
+    </div>
   )
 }
 
